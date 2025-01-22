e6ed87299d31d81c5fe9b78e7e45248af7791efc
Torbjörn Törnkvist <kruskakli@gmail.com>
2025-01-22 10:07:09 +0100

    Adding terminal_size.c to compare with the Zig code

--- build.zig
+++ build.zig
@@ -82,6 +82,16 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    // Terminal size executable
+    const terminal_size = b.addExecutable(.{
+        .name = "terminal_size",
+        .target = target,
+        .optimize = optimize,
+    });
+    terminal_size.addCSourceFile(.{ .file = b.path("src/terminal_size.c"), .flags = &.{} });
+    terminal_size.linkLibC();
+    b.installArtifact(terminal_size);
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
